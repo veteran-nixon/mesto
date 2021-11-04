36d649cab7758717e5d0c5939dbd958561c5cf1d
Evgeny Dolganev <dolganev007@yandex.ru>
2021-11-04 13:14:58 +0700

    fix: 1.исправить классы по бэм, 2. исправить открытие и закрытие попап добавлением модификатора popup_opened

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let nameParagraph = document.querySelector('.profile__info-name');  //поле имение
-let bioParagraph = document.querySelector('.profile__info-bio'); //поле о себе
-let nameInput = document.querySelector('.edit-form__name'); //инпут имени
-let bioInput = document.querySelector('.edit-form__bio');   //инпут о себе
+let nameParagraph = document.querySelector('.profile__name');  //поле имение
+let bioParagraph = document.querySelector('.profile__bio'); //поле о себе
+let nameInput = document.querySelector('.popup__name'); //инпут имени
+let bioInput = document.querySelector('.popup__bio');   //инпут о себе
 
 // открыть окно редактирования профиля
 let editButton = document.querySelector('.profile__edit-button');
 
 function addPopup() {
-    let popup = document.querySelector('.profile__overlay');
-    popup.setAttribute('style', 'display:flex');
+    let popup = document.querySelector('.popup');
+    popup.classList.add('popup_opened');
     nameInput.value = nameParagraph.textContent;
     bioInput.value = bioParagraph.textContent;
 }
@@ -16,25 +16,25 @@ function addPopup() {
 editButton.addEventListener('click', addPopup);
 
 // закрыть окно редактирования профиля нажатием на крестик
-let closeButton = document.querySelector('.edit-form__close-icon');
+let closeButton = document.querySelector('.popup__close-icon');
 
 function closePopup() {
-    let popup = document.querySelector('.profile__overlay');
-    popup.removeAttribute('style', 'display:flex');
+    let popup = document.querySelector('.popup');
+    popup.classList.remove('popup_opened');
 }
 
 closeButton.addEventListener('click', closePopup);
 
 // закрыть окно редактирования профиля нажатием на любую часть экрана вне попап
 window.addEventListener('click', function(event) {
-    let popup = document.querySelector('.profile__overlay');
+    let popup = document.querySelector('.popup');
     if (event.target == popup) {
-        popup.removeAttribute('style', 'display:flex');
+        popup.classList.remove('popup_opened');
     }
 });
 
 // сохранить поля отправки попап нажатием на кнопку "сохранить"
-let formElement = document.querySelector('.edit-form__submit-button');
+let formElement = document.querySelector('.popup__submit-button');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
